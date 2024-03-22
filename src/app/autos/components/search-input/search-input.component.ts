3bcd04b9e6a0bfd6347d34c1a771982a3c85f201
nsandoya <natsandoya@gmail.com>
2024-03-21 23:15:54 -0500

    Search: restaurar lista original después de búsqueda -> hecho

--- src/app/autos/components/search-input/search-input.component.ts
+++ src/app/autos/components/search-input/search-input.component.ts
@@ -16,21 +16,6 @@ export class SearchInputComponent {
   constructor(private listaService: AutosInfo){}
 
   ngOnInit(){
-    //this.autos = this.listaService
-    /* this.listaService.listaPrincipal.subscribe((autos:any)=>{
-      this.autos = autos
-
-    }) */
-    
-  
-   /*  this.listaService.getFilteredList().subscribe((autos)=>{
-      this.filterList = autos
-    }) */
-    /* this.listaService.getItemById(this.searchCr).subscribe((autos:any)=>{
-      this.filterList = autos
-    }) */
-
-    //this.filterList = this.listaService.searchItem(this.searchCr);
   }
   
  /*  searchItem(){
@@ -40,22 +25,26 @@ export class SearchInputComponent {
   searchItem(){
     this.listaService.getCompleteList(this.searchCr).subscribe((autos)=>{
       this.autos = autos
+      this.listaService.refreshList(this.autos)
+      /* console.log("Búsqueda 2.0",...this.autos)
+      console.log("Refresh",this.listaService.refreshList(this.autos)) */
+      
+    })
+  }
+
+  returnAllItems(){
+    this.listaService.getCompleteList().subscribe((autos)=>{
+      this.autos = autos
       
       console.log("Búsqueda 2.0",...this.autos)
       console.log("Refresh",this.listaService.refreshList(this.autos))
       
     })
+  }
 
-  
-    /* this.listaService.searchItem(this.searchCr).subscribe((autos:any)=>{
-      this.autos = autos */
-      
-    console.log("search", ...this.autos)
-    /* this.filterList = this.listaService.searchItem(this.searchCr);
-    console.log("lista filtrada", this.filterList) */
-  //})
-  
-  /* getFilteredList(){
-    this.listaService.getFilteredList()
-  } */
-}}
+  noValue(){
+    if (!this.searchCr){
+      this.returnAllItems()
+    }
+  }
+}
