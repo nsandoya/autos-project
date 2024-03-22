850d9a205099cc72137a33f59312a86cc486cf68
nsandoya <natsandoya@gmail.com>
2024-03-22 00:40:55 -0500

    Card navbar: se añaden clases dinámicamente, según el tab activo

--- src/app/autos/components/auto-detail-page/auto-detail-page.component.ts
+++ src/app/autos/components/auto-detail-page/auto-detail-page.component.ts
@@ -1,4 +1,4 @@
-import { ViewChild,Component, OnInit, TemplateRef } from '@angular/core';
+import { ViewChild,Component, OnInit, TemplateRef, AfterViewInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AutosInfo } from '../../services/autos-info.service';
 import { ListaInterface } from '../../interfaces/lista-interface';
@@ -8,13 +8,13 @@ import { ListaInterface } from '../../interfaces/lista-interface';
   templateUrl: './auto-detail-page.component.html',
   styleUrl: './auto-detail-page.component.css'
 })
-export class AutoDetailPageComponent implements OnInit{
+export class AutoDetailPageComponent implements OnInit, AfterViewInit{
   auto: ListaInterface | any ;
   @ViewChild('main') main!: TemplateRef<any>;
   @ViewChild('detail1') detail1!: TemplateRef<any>;
   @ViewChild('detail2') detail2!: TemplateRef<any>;
 
-  activeTab!: TemplateRef<any>;
+  activeTab!: TemplateRef<any> /* = this.main */;
   // ActivatedRoute crea una instancia de clase que incluye, entre otras cosas, info asociada a un componente, cargada al route outlet
   // En mi caso, TablaPrincipal está cargada al route outlet, y al hacer click en uno de sus elementos, esta envía info (el id del auto) al routeLink.
   // Por ende, nosotros buscamos obtener ese dato para usarlo acá
@@ -22,7 +22,13 @@ export class AutoDetailPageComponent implements OnInit{
 
   }
 
+  ngAfterViewInit(): void {
+    
+    this.activeTab = this.main ;
+  }
+
   ngOnInit(): void {
+
     this.route.params.subscribe((params)=>{
       const id = params['id'];
       console.log(id)
